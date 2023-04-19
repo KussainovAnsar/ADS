797a543c113cb655cc0643230e8facdacd6de0ae
Ансар <119420509+KussainovAnsar@users.noreply.github.com>
2023-04-19 20:57:23 +0600

    Update MyArrayList.java

--- week2/MyArrayList.java
+++ week2/MyArrayList.java
@@ -7,17 +7,19 @@ public abstract class MyArrayList<T> implements MyList<T> {
 
     private Object[] arrayElements;
     private int size;
-
+    
     public MyArrayList() {
         this.arrayElements = new Object[capacity];
         this.size = 0;
-    }
+    }   // Constructor that initializes the arrayElements
 
     @Override
     public int size() {
         return size;
-    }
+    } // Returns the number of elements in the ArrayList
 
+    
+    // Returns true if the list contains the specific element
     @Override
     public boolean contains(Object o) {
         for (int i = 0; i < size; i++) {
@@ -29,8 +31,8 @@ public abstract class MyArrayList<T> implements MyList<T> {
     }
 
     @Override
-    public void add(T item) {
-        if (size == arrayElements.length) {
+    public void add(T item) {   // Adds a specified item to the end of the list
+        if (size == arrayElements.length) {    // If the array is full, increase its size
             Object[] newElements = new Object[2 * arrayElements.length];
             System.arraycopy(arrayElements, 0, newElements, 0, arrayElements.length);
             arrayElements = newElements;
@@ -40,7 +42,7 @@ public abstract class MyArrayList<T> implements MyList<T> {
     }
 
     @Override
-    public void add(T item, int index) {
+    public void add(T item, int index) {      // method adds the specified item to the list at the specified index.
         if (index < 0 || index > size) {
             throw new IndexOutOfBoundsException();
         }
@@ -55,7 +57,7 @@ public abstract class MyArrayList<T> implements MyList<T> {
     }
 
     @Override
-    public boolean remove(T item) {
+    public boolean remove(T item) {       // method removes the specified item from the arrayList.
         for (int i = 0; i < size; i++) {
             if (arrayElements[i].equals(item)) {
                 remove(i);
@@ -66,7 +68,7 @@ public abstract class MyArrayList<T> implements MyList<T> {
     }
 
     @Override
-    public T remove(int index) {
+    public T remove(int index) {               // method removes the item at the specified index from the ArrayList.
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException();
         }
@@ -78,21 +80,23 @@ public abstract class MyArrayList<T> implements MyList<T> {
     }
 
     @Override
-    public void clear() {
+    public void clear() {                    // method removes all elements from the list.
         arrayElements = new Object[capacity];
         size = 0;
     }
 
     @Override
-    public T get(int index) {
+    public T get(int index) {               // method returns the item at the specified index.
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException();
         }
         return (T) arrayElements[index];
     }
-
+     
+    // method returns the index of the first occurrence of the specified element in the list, or -1 if the list does not contain the element.
+    // it iterates through the list and compares each element to the specified object using the equals() method.
     @Override
-    public int indexOf(Object o) {
+    public int indexOf(Object o) {       
         for (int i = 0; i < size; i++) {
             if (arrayElements[i].equals(o)) {
                 return i;
@@ -100,7 +104,9 @@ public abstract class MyArrayList<T> implements MyList<T> {
         }
         return -1;
     }
-
+    
+    // method returns the index of the last occurrence of the specified element in the list, or -1 if the list does not contain the element.
+    // it iterates through the list backwards and compares each element to the specified object using the equals() method.
     @Override
     public int lastIndexOf(Object o) {
         for (int i = size - 1; i >= 0; i--) {
@@ -110,22 +116,10 @@ public abstract class MyArrayList<T> implements MyList<T> {
         }
         return -1;
     }
-
+    
+    // method sorts the elements in the list in ascending order using the Arrays.sort() method. It only sorts the elements up to the current size of the list.
     @Override
     public void sort() {
         Arrays.sort(arrayElements, 0, size);
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder("[");
-        for (int i = 0; i < size; i++) {
-            sb.append(arrayElements[i]);
-            if (i < size - 1) {
-                sb.append(", ");
-            }
-        }
-        sb.append("]");
-        return sb.toString();
     }
 }
